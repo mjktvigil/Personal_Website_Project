75150f5aeaeb96c5e6c1d7d58d5052293a9ce75d
Marie Vigil <mjkvigil@gmail.com>
2015-07-20 00:27:17 -0600

    modified Configurations

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang=“en”>
+
+	<head>
+		<meta charset="utf-8">
+		<title>marie jt vigil personal website</title>
+		<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+	</head>
+
+	<body>
+
+
+
+	</body>
+
+</html>
