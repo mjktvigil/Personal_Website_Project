510801c013f46fd925b5a1e59cff47c3ae96afaa
Marie Vigil <mjkvigil@gmail.com>
2015-08-10 23:27:58 -0600

    milestone-3
    restructured directory

--- js/myscript.js
+++ js/myscript.js
@@ -8,32 +8,13 @@ x = 0;
 $(document).ready(function(){
 
 	// scroll
-	$("blockquote").scroll(function(){
+	$("chat-with-us").scroll(function(){
 	});
 
-	// carousel
+	/** carousel
 	$('.carousel').carousel({
 		interval: 5000
-	});
-
-	// scroller
-	$(function(){
-		var scroller = $('#scroller div.innerScrollArea');
-		var scrollerContent = scroller.children('ul');
-		scrollerContent.children().clone().appendTo(scrollerContent);
-		var curX = 0;
-		scrollerContent.children().each(function(){
-			var $this = $(this);
-			$this.css('left', curX);
-			curX += $this.outerWidth(true);
-		});
-		var fullW = curX / 2;
-		var viewportW = scroller.width();
-
-		scroller.css('overflow-x', 'auto');
-	});
-
-
+	}); **/
 
 
 
