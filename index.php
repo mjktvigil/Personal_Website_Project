de9c30d2048716ccc19375108eb694948003c4fd
Marie Vigil <mjkvigil@gmail.com>
2015-08-13 16:55:28 -0600

    cleaned up indentations to all lib directory files

--- index.php
+++ index.php
@@ -3,24 +3,20 @@
 	<div class="container">
 		<!-------- HEADER -------->
 		<header>
-			<?php require_once(dirname(__DIR__) . "/personal_website_project/lib/header.php"); ?>
+			<?php require_once(dirname(__DIR__) . "lib/header.php"); ?>
 		</header>
-
 		<div class="row">
 			<section class="side-panel col-md-3">
 			</section>
-
 			<main class="main-content col-md-9">
 				<h1>Hello World</h1>
 				<p>This is the main page content here.</p>
 			</main>
 		</div><!-- row -->
-
 		<!-------- Footer -------->
 		<footer class="footer">
-			<?php require_once(dirname(__DIR__) . "/personal_website_project/lib/footer.php"); ?>
+			<?php require_once(dirname(__DIR__) . "lib/footer.php"); ?>
 		</footer>
-
 	</div><!-- .container -->
 </body>
 </html>
--- lib/footer.php
+++ lib/footer.php
@@ -1,6 +1,7 @@
 
-
-<div class="content container-fluid">
+<!-------- FOOTER -------->
+	<nav class="navbar" role="navigation">
+		<div class="content container-fluid">
 			<div class="row">
 				<div class="col-sm-6">
 					<p>Call us toll-free at <span class="phone">000-000-000</span></p>
@@ -8,8 +9,8 @@
 				<div class="row">
 					<div class="col-sm-6">
 						<p>All contents &copy; 2015 <a href="   ">xxxx</a></p>
-						</ul>
-					</nav>
-				</div><!-- col-sm-6 -->
+					</div><!-- col-sm-6 -->
+				</div><!-- row -->
 			</div><!-- row -->
 		</div><!-- content container -->
+	</nav><!-- navbar -->
