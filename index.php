e3a7a4e9508b1195ab53d7b45055dd7072d6760f
Marie Vigil <mjkvigil@gmail.com>
2015-08-13 16:11:59 -0600

    cleaned up indentations

--- index.php
+++ index.php
@@ -1,56 +1,26 @@
-<?php require_once(dirname(__DIR__) . "/personal_website_project/lib/head-utils.php"); ?>
+<?php require_once("lib/head-utils.php"); ?>
+<body>
+	<div class="container">
+		<!-------- HEADER -------->
+		<header>
+			<?php require_once(dirname(__DIR__) . "/personal_website_project/lib/header.php"); ?>
+		</header>
 
+		<div class="row">
+			<section class="side-panel col-md-3">
+			</section>
 
-	<!-------- HEADER -------->
+			<main class="main-content col-md-9">
+				<h1>Hello World</h1>
+				<p>This is the main page content here.</p>
+			</main>
+		</div><!-- row -->
 
-<div class="container">
+		<!-------- Footer -------->
+		<footer class="footer">
+			<?php require_once(dirname(__DIR__) . "/personal_website_project/lib/footer.php"); ?>
+		</footer>
 
-	<header>
-
-		<?php require_once(dirname(__DIR__) . "/personal_website_project/lib/header.php"); ?>
-
-	</header>
-
-
-<!-------- Body -------->
-
-	<body>
-
-
-	<div class="row">
-		<section class="side-panel col-md-3">
-
-
-
-		</section>
-
-		<section class="main-content col-md-9">
-			<h1>Hello World</h1>
-			<p>This is the main page content here.</p>
-		</section>
-	</div><!-- row -->
-
-
-
-	<!-------- Footer -------->
-
-
-	<footer class="footer">
-
-		<?php require_once(dirname(__DIR__) . "/personal_website_project/lib/footer.php"); ?>
-
-	</footer>
-
-
-
-
-
-
-</div><!-- .container -->
-
-
+	</div><!-- .container -->
 </body>
-
-
-
 </html>
