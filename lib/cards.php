2ebbe7509490a546a5916e43c3e96231ce19cb21
Marie Vigil <mjkvigil@gmail.com>
2015-08-16 11:50:53 -0600

    fixed desktop version by fixing <section> tags (removed extra and moved closing section tag down below end card three area)

--- lib/cards.php
+++ lib/cards.php
@@ -13,11 +13,12 @@
 	</head>
 
 	<body>
+		<div class="outer-wrap">
 <!-- Masthead -->
-		<header class="masthead clear">
+		<header class="masthead">
 			<div class="centered">
 <!-- Banner image -->
-				<div class="branding image">
+				<div class="site-branding">
 <!--				<a href="#">
 						<h1 class="site-title">jtdesignsolutions-Card Layout</h1>-->
 						<img src="?" alt="?" title="?">
@@ -29,48 +30,51 @@
 		<main class="main">
 			<div class="centered">
 <!-- Start Cards -->
-<!-- Card One -->
 				<section class="cards">
+<!-- Card One -->
 					<article class="card">
 						<a href="#">
-							<h2>Outbound Marketing</h2>
-								<figure class="thumbnail">
+							<h2>Marketing Resources</h2>
+							<figure class="thumbnail">
 								<img src="../gallery/img/fpo-gray-box.jpg" alt="?" title="?">
-								</figure>
-								<div class="card-content">
-									<p>???</p>
-								</div><!-- .card-content -->
+							</figure>
+							<div class="card-content">
+								<p>???</p>
+							</div><!-- .card-content -->
 						</a>
 					</article><!-- .card -->
 <!-- Card Two -->
-					<section class="cards">
+					<article class="card">
+						<a href="#">
+							<h2>Marketing Resources</h2>
+							<figure class="thumbnail">
+								<img src="../gallery/img/fpo-gray-box.jpg" alt="?" title="?">
+							</figure>
+							<div class="card-content">
+								<p>???</p>
+							</div><!-- .card-content -->
+						</a>
+					</article><!-- .card -->
+<!-- Card Three -->
 						<article class="card">
 							<a href="#">
-								<h2>Inbound Marketing</h2>
-								<figure class="thumbnail">
-									<img src="../gallery/img/fpo-gray-box.jpg" alt="?" title="?">
-								</figure>
-								<div class="card-content">
-									<p>???</p>
-								</div><!-- .card-content -->
-							</a>
-						</article><!-- .card -->
-<!-- Card Three -->
-						<section class="cards">
-							<article class="card">
-								<a href="#">
-									<h2>Marketing Resources</h2>
+								<h2>Marketing Resources</h2>
 									<figure class="thumbnail">
 										<img src="../gallery/img/fpo-gray-box.jpg" alt="?" title="?">
 									</figure>
 									<div class="card-content">
 										<p>???</p>
 									</div><!-- .card-content -->
-								</a>
-							</article><!-- .card -->
+							</a>
+						</article><!-- .card -->
+<!-- End Card Three -->
+					</section><!-- .cards -->
 <!-- End Cards -->
-				</section><!-- .cards -->
-			</div><!-- .centered -->
+				</div><!-- .centered -->
 		</main>
+			<footer class="footer-area">
+				<p>The footer usually appears on the bottom of the page. That's why it's called the "footer".</p>
+			</footer>
+		</div><!-- .outer-wrap -->
 	</body>
 </html>
