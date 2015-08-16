2396f1cf7f7da1708e7ac3603fe986a969e44df1
Marie Vigil <mjkvigil@gmail.com>
2015-08-16 10:20:30 -0600

    uploading card.html and card-styles.css will remove html head and head-utils from html file later after working out the layout

---
+++ lib/cards.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>jtdesignsolutions-Card Layout</title>
+		<meta name="description" content="Flexible box card-based layout demo from Morten Rand-Hendriksen, staff author at lynda.com"/>
+<!-- Custom Css -->
+		<link rel="stylesheet" href="../css/card-styles.css" type="text/css" media="all">
+<!-- Bootstrap and Font-awesome (current version???) -->
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+	</head>
+
+	<body>
+<!-- Masthead -->
+		<header class="masthead clear">
+			<div class="centered">
+<!-- Banner image -->
+				<div class="branding image">
+<!--					<h1 class="site-title">jtdesignsolutions-Card Layout</h1>-->
+					<img src="?" alt="?" title="?">
+				</div><!-- .branding image -->
+			</div><!-- .centered -->
+		</header><!-- .masthead -->
+<!-- Main -->
+		<main class="main-area">
+			<div class="centered">
+<!-- Start Cards -->
+<!-- Card One -->
+				<section class="cards">
+					<article class="card">
+						<a href="#">
+							<h2>Outbound Marketing</h2>
+								<figure class="thumbnail">
+								<img src="?" alt="?" title="?">
+								</figure>
+								<div class="card-content">
+									<p>???</p>
+								</div><!-- .card-content -->
+						</a>
+					</article><!-- .card -->
+<!-- Card Two -->
+					<section class="cards">
+						<article class="card">
+							<a href="#">
+								<h2>Outbound Marketing</h2>
+								<figure class="thumbnail">
+									<img src="?" alt="?" title="?">
+								</figure>
+								<div class="card-content">
+									<p>???</p>
+								</div><!-- .card-content -->
+							</a>
+						</article><!-- .card -->
+<!-- Card Three -->
+						<section class="cards">
+							<article class="card">
+								<a href="#">
+									<h2>Outbound Marketing</h2>
+									<figure class="thumbnail">
+										<img src="?" alt="?" title="?">
+									</figure>
+									<div class="card-content">
+										<p>???</p>
+									</div><!-- .card-content -->
+								</a>
+							</article><!-- .card -->
+<!-- End Cards -->
+				</section><!-- .cards -->
+			</div><!-- .centered -->
+		</main>
+	</body>
+</html>
