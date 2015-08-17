ed8e7e55a817384638f8ef77ca6a057f4156eb85
Marie Vigil <mjkvigil@gmail.com>
2015-08-16 22:53:46 -0600

    uploading...finished...worked on chat with us page...for tonight

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,89 @@
-<?php
+<?php require_once('../lib/head-utils.php'); ?>
+<link href="../css/card-styles.css" rel="stylesheet" type="text/css" media="all">
+
+	<!-------- HEADER -------->
+	<header>
+		<?php require_once('../lib/header.php'); ?>
+	</header>
+	<body>
+		<!-- Banner image -->
+		<div class="masthead">
+<!--		<a href="#">
+				<h1 class="site-title">jtdesignsolutions-Card Layout</h1>-->
+<!--		</a>-->
+		</div><!-- .masthead -->
+		<!-- Main -->
+		<main class="main">
+		<div class="container-fluid">
+
+			<div class="centered">
+<!-- Start Cards -->
+				<section class="cards">
+<!-- Card One -->
+					<article class="card">
+						<a href="outbound-marketing.php">
+							<h2 class="click-header1">Outbound Marketing</h2>
+							<figure class="thumbnail">
+								<img src="../gallery/img/fpo-gray-box.jpg" alt="?" title="?">
+							</figure>
+							<div class="card-content">
+								<p>???</p>
+							</div><!-- .card-content -->
+							<div class="card-line1">
+							</div>
+						</a>
+					</article><!-- .card -->
+<!-- Card Two -->
+					<article class="card">
+						<a href="inbound-marketing.php">
+							<h2 class="click-header2">Inbound Marketing</h2>
+							<figure class="thumbnail">
+								<img src="../gallery/img/fpo-gray-box.jpg" alt="?" title="?">
+							</figure>
+							<div class="card-content">
+								<p>???</p>
+							</div><!-- .card-content -->
+							<div class="card-line2">
+							</div>
+						</a>
+					</article><!-- .card -->
+<!-- Card Three -->
+					<article class="card">
+						<a href="resources.php">
+							<h2 class="click-header3">Marketing Resources</h2>
+								<figure class="thumbnail">
+									<img src="../gallery/img/fpo-gray-box.jpg" alt="?" title="?">
+								</figure>
+								<div class="card-content">
+										<p>???</p>
+								</div><!-- .card-content -->
+							<div class="card-line3">
+							</div>
+						</a>
+						</article><!-- .card -->
+<!-- End Card Three -->
+					</section><!-- .cards -->
+				</div><!-- .centered -->
+		</div><!-- .container-fluid -->
+		</main>
+<!-- bottom-line -->
+		<section class="bottom-line">
+			<div class="container-fluid">
+				<div class="row-fluid">
+				</div>
+			</div>
+		</section><!-- .bottom-line -->
+		<footer class="footer-area">
+			<div class="container-fluid">
+				<div class="row-fluid">
+				</div>
+			</div>
+			<div class="footer-toggle">
+				<div class="container-fluid">
+					<div class="row-fluid">
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
