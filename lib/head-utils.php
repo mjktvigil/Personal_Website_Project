3f56bc3e619d99853da6cc3929c74826445fcf1a
Marie Vigil <mjkvigil@gmail.com>
2015-08-26 15:07:39 -0600

    uploading project to github

--- lib/head-utils.php
+++ lib/head-utils.php
@@ -54,6 +54,6 @@
 
 	  <!--JavaScript and CSS -->
 
-	  <title>marie j.t. vigil |jtdesignsolutions | production coordinator • interaction designer • production artist</title>
+	  <title>jtdesignsolutions</title>
 
   </head>
