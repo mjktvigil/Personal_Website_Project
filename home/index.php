d2e59a5f07f9593549fda47dccc31124ee3b9bfa
Marie Vigil <mjkvigil@gmail.com>
2015-09-11 15:37:05 -0600

    fixed typos and grammar on home page

--- home/index.php
+++ home/index.php
@@ -21,7 +21,7 @@
 				<h2>As a Designer&#44; I engineer design solutions</h2>
 				<p class="">
 					that stay focused​ on the message&#44; seek opportunities​ for redefining common solutions&#44; ask questions that seek deeper understanding&#44; welcome risk-taking&#44; and demystify constraints.
-					For me&#44; design is more than creating a pretty picture. The design solution must have a purpose and value to it&rsquo;s end-user&hellip;and be simple to use.
+					Design is more than creating a pretty picture. The design solution must have a purpose and value to it&rsquo;s end-user&hellip;and be simple to use.
 					Check out the three areas that I specialize in as a Design Engineer.
 				</p>
 				&nbsp;
@@ -47,16 +47,15 @@
 									<img src="../img/woman-using-computer.jpg" alt="???" />
 								</div>
 								<div class="box-gray">
-									<h4 class="aligncenter">InBound Marketing?<br/> Huh, what?</h4>
+									<h4 class="aligncenter">InBound Marketing<br/> What's that?</h4>
 									<p>
 									Inbound Marketing can be defined as marketing activities that bring visitors IN, rather than marketers going OUT to get protential client's attention.
-									Inbound Marketing uses the world-wide-web to advertise your message or busines. Clients are attracted to your online presents by knowledgeable, informative, or valued content, easily found search engine links or key words,
+									Inbound Marketing uses the world-wide-web to advertise your business. Clients are attracted to your online presents by knowledgeable, informative, or valued content, easily found search engine links or key words,
 									shared content on social media networks, and personalized emails.
 									</p>
 									<p>
 									Inbound Marketing is effective for both small and large businesses as
-									an affordable method of reaching a larger amount of local, national, or international users
-									than offered by traditional direct marketing.
+									an affordable method of reaching a larger audience locally, nationally, and internationally.
 									</p>
 									<p>
 									In short, Inbound Marketing creates on-demand experiences for your
@@ -86,11 +85,11 @@
 									<img src="../img/magazine-rack.jpg" alt="???" />
 								</div>
 								<div class="box-gray">
-									<h4 class="aligncenter">OutBound Marketing&mdash;<br/>how traditional!</h4>
+									<h4 class="aligncenter">OutBound Marketing<br/>How traditional.</h4>
 									<p>
 									Outbound Marketing, or traditional marketing, can include printed advertisements, such as newsletters, billboards, flyers and magazine
-									print ads. Other forms of traditional marketing include television commercials, as well as radio spots
-									advertising a business, product, or service. Printed posters utilized as signage inform viewers of limited promotions. With traditional marketing, anyone with a newspaper, mail service, television, radio or sitting at a coffee shop can learn of your business.
+									print ads. Other forms of traditional marketing include television commercials, as well as radio spots that
+									advertise your business, product, or service. With traditional marketing, anyone with a newspaper, mail service, television, or radio can learn about your business.
 									As a business using traditional marketing, you bring your message to potential customers.
 									</p>
 									<p>
@@ -98,7 +97,7 @@
 									create brand recongition and increase your online presence by advertising your blog or social media networks.
 									</p>
 									<p>
-									It's hard to visualize that traditional marketing retains a place in spreading the word, however, traditional marketing remains a tool of communication.
+									It's hard to visualize that traditional marketing retains a place in spreading the word, however, traditional marketing remains a prefered choice of communication for many customers.
 									</p>
 									<div class="aligncenter">
 										<a class="btn-theme" alt="See more" href="../outbound-marketing/index.php" title="click to See More">OutBound Marketing Portfolio</a>
@@ -125,10 +124,12 @@
 									<p>
 									Well, first let&rsquo;s look at what is a Cognitive Marketing approach to Design.
 									Simply put, Cognitive Design means to design with intent&mdash;to solve problems by thinking about the situation from many view points.
-									In Cognitive Design, a designer approaches problem solving as an engineer, rather than an artist, providing a practical set of collaborative tools
-									and methods for planning and defining a successful designed product.
+									In Cognitive Design, a designer approaches problem solving as an engineer, rather than an artist.
 									The traditional thought that a square peg will fit into a round hole or a one-size-fits-all design process will not develop into innovatative solutions.
-									One answer, cause we have always done it that way, is not the perfect fit for many situations. With Cognitive Design or Design Thinking many possible answers are created and evaluated
+									The popular answer &ldquo;cause we have always done it that way&ldquo; is not the perfect fit for many situations.
+									</p>
+									<p>
+									With Cognitive Design or Design Thinking many possible answers are created and evaluated
 									for one solution to one root-cause situation.
 									</p>
 									<p>
