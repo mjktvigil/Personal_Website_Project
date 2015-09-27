0bb3c5ba99dac5c2f1ebec4e6d8adb88b73b3b78
Marie Vigil <mjkvigil@gmail.com>
2015-09-26 21:18:05 -0600

    Created a Suggested Reading list placeholder

--- test/index.php
+++ test/index.php
@@ -12,8 +12,26 @@
 								<li class="example-menu2"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
 								<li class="example-menu3"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
 							</ul>
-						</nav>
-						<!-- .example-menu -->
+						</nav><!-- .example-menu -->
+						<div class="suggested-reading">
+							<h3>Suggested Reading</h3>
+							<nav class="example-menu">
+								<ul>
+									<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
+									<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
+									<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
+									<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
+									<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
+									<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
+									<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
+									<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
+									<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
+									<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
+									<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
+									<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
+								</ul>
+						</nav><!-- .example-menu -->
+						</div>
 					</div>
 					<!-- .squish-container -->
 				</div>
@@ -35,20 +53,21 @@
 						</p>
 						<p class="paragraph">
 							As designers within a business,  it’s our job to solve a business problem first, than create a thing of beauty second.
-							We need to frame design problems as business problems as well as identify concerns though a scientific microscope.
-							Why and how does a design of an interface or view benefit the user AND create a Return On Investment (ROI) for the company.
+							We need to frame design problems as business problems as well as dissect user concerns though a scientific microscope.
+							Collaboration with subject matter experts, stakeholders, and research of user's needs will help identify what features and functions will result in a
+							great user experience AND return on investment for both the user and the business.
 						</p>
 						<p class="paragraph">
 							Product development and design is a cyclical process. Creativity during the innovation phase is often based on user experience and artistry.
-							Collected data during the innovation phase can direct designers and developers to a broken step in the user flow.
-							Data that directs your attention to the problem won’t tell us how to fix the problem but aids us in finding a solution.
+							Collected data during the investigation phase can direct designers and developers to a broken step in the user flow.
+							Data that directs our attention to the problem won’t tell us how to fix the problem but aids us in finding a solution.
 						</p>
 						<p class="paragraph">
-							Data and analysis should ultimately be part of the design process and not just a marketing process.
-							Designers can’t afford to rely on their instincts alone. Designers can learn from other departments wealth of data.
+							Data, analysis, and strategic planning should ultimately be part of the design process.
+							Designers can learn from other departments wealth of data.
 							Applying the right data at the right time within the right design stage is an important element to the future of design.
 							Marketing, design, and development go hand-in-hand in building, launching, and selling a successful product.
-							That is a cognitive marketing approach to design.
+							Seeking understanding of the complexities of everyday human experience and merging them with business goals leads to a cognitive marketing approach to design.
 						</p>
 					<div class="white-space"></div>
 				</article>
@@ -64,20 +83,9 @@
 	<section class="container read-more">
 		<div class="row">
 			<div class="col-md-4">
-				<div class="profile">
-					<div class="example-menu">
-						<h3>Suggested Reading</h3>
-						<ul>
-							<li class="example-menu4"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
-							<li class="example-menu2"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
-							<li class="example-menu3"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
-						</ul>
-					</div>
-				</div>
-				<!-- profile -->
 			</div>
 			<!-- col-md-4 -->
-			<!-- Start Family History -->
+			<!-- Start Focus-paragraph -->
 			<div class="col-md-8">
 				<artilce class="post-content3">
 					<h2>Staying Focused</h2>
@@ -87,12 +95,12 @@
 						Scope creep is harmful to the project and often causes damage to a team environment.
 						Collaboration, mutual respect, open communication, and teamwork are the keys to successful problem
 						solving as much as asking the right questions and doing the right amount of research at the beginning of the project.
-						Don't let scope creep complicate the design and development process. Know when to say no and when to make the right except for the right reason.
+						Don't let scope creep complicate the design and development process. Know when to say no and when to make the right exception for the right reason.
 					</p>
 				</artilce>
 			</div>
 			<!--.col-md-8 -->
-			<!-- End Family History -->
+			<!-- End Focus-paragraph -->
 		</div>
 		<!--.row -->
 	</section>
