514c6171cbe5c70e51751eba1387e5f0824a0747
Marie Vigil <mjkvigil@gmail.com>
2015-09-26 22:36:20 -0600

    Book titles and Amazon links placed in HTML file

--- test/index.php
+++ test/index.php
@@ -15,25 +15,26 @@
 						</nav><!-- .example-menu -->
 						<div class="suggested-reading">
 							<h3>Suggested Reading</h3>
+							<h4>Design and UX:</h4>
 							<nav class="example-menu">
 								<ul>
-									<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
-									<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
-									<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
-									<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
-									<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
-									<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
-									<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
-									<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
-									<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
-									<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
-									<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
-									<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
-									<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
-									<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
-									<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
-									<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
-									<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Design-Everyday-Things-Revised-Expanded/dp/0465050654/ref=sr_1_1?s=books&ie=UTF8&qid=1443326931&sr=1-1&keywords=The+Design+of+Everyday+Things" alt="link to Amazon" target="_blank"><strong>The Design of Everyday Things</strong></a> </li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Rocket-Surgery-Made-Easy--Yourself/dp/0321657292/ref=sr_1_1?s=books&ie=UTF8&qid=1443326964&sr=1-1&keywords=Rocket+Surgery+Made+Easy" alt="link to Amazon" target="_blank"><strong>Rocket Surgery Made Easy</strong></a></li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Dont-Make-Think-Revisited-Usability/dp/0321965515/ref=sr_1_2?s=books&ie=UTF8&qid=1443326964&sr=1-2&keywords=Rocket+Surgery+Made+Easy" alt="link to Amazon" target="_blank"><strong>Don't Make Me Think 3rd edition</strong></a></li>
+									<li class="reading-menu"><a href="http://www.amazon.com/101-Design-Methods-Structured-Organization/dp/1118083466/ref=sr_1_1?s=books&ie=UTF8&qid=1443327037&sr=1-1&keywords=101+Design+Methods" alt="link to Amazon" target="_blank"><strong>101 Design Methods</strong></a> </li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Designing-Web-Usability-Jakob-Nielsen/dp/156205810X/ref=sr_1_1?s=books&ie=UTF8&qid=1443327060&sr=1-1&keywords=Designing+Web+Usability" alt="link to Amazon" target="_blank"><strong>Designing Web Usability</strong></a></li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Dieter-Rams-Better-English-German/dp/3899555252/ref=sr_1_1?s=books&ie=UTF8&qid=1443327130&sr=1-1&keywords=Dieter+Rams.+Less+but+Better" alt="link to Amazon" target="_blank"><strong>Dieter Rams. Less but Better</strong></a></li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Laws-Simplicity-Design-Technology-Business/dp/0262134721/ref=sr_1_1?s=books&ie=UTF8&qid=1443327281&sr=1-1&keywords=Laws+of+Simplicity" alt="link to Amazon" target="_blank"><strong>Laws of Simplicity</strong></a> </li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Thinking-Fast-Slow-Daniel-Kahneman/dp/0374533555/ref=sr_1_1?s=books&ie=UTF8&qid=1443327322&sr=1-1&keywords=Thinking%2C+Fast+and+Slow" alt="link to Amazon" target="_blank"><strong>Thinking, Fast and Slow</strong></a></li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Predictably-Irrational-Revised-Expanded-Decisions/dp/0061353248/ref=sr_1_1?s=books&ie=UTF8&qid=1443327394&sr=1-1&keywords=Predictably+Irrational" alt="link to Amazon" target="_blank"><strong>Predictably Irrational</strong></a></li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Art-Possibility-Transforming-Professional-Personal/dp/0142001104/ref=sr_1_1?s=books&ie=UTF8&qid=1443327452&sr=1-1&keywords=The+Art+of+Possibility" alt="link to Amazon" target="_blank"><strong>The Art of Possibility</strong></a> </li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Breakfast-Buddha-Roland-Merullo/dp/1565126165/ref=sr_1_1?s=books&ie=UTF8&qid=1443327481&sr=1-1&keywords=breakfast+with+buddha" alt="link to Amazon" target="_blank"><strong>Breakfast with Buddah</strong></a></li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Life-Pi-Yann-Martel/dp/0156027321/ref=sr_1_1?s=books&ie=UTF8&qid=1443327539&sr=1-1&keywords=Life+of+Pi" alt="link to Amazon" target="_blank"><strong>Life of Pi</strong></a></li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Where-Wild-Things-Maurice-Sendak/dp/0064431789" alt="link to Amazon" target="_blank"><strong>Where the Wild Things Are</strong></a> </li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Tao-Pooh-Benjamin-Hoff/dp/0140067477/ref=sr_1_1?s=books&ie=UTF8&qid=1443327758&sr=1-1&keywords=The+Tao+of+Pooh" alt="link to Amazon" target="_blank"><strong>The Tao of Pooh</strong></a></li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Really-Need-Know-Learned-Kindergarten/dp/034546639X/ref=sr_1_1?s=books&ie=UTF8&qid=1443327795&sr=1-1&keywords=all+i+really+need+to+know+i+learned+in+kindergarten" alt="link to Amazon" target="_blank"><strong>All I Really Need To Know I Learned in Kidergarten</strong></a> </li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Was-Fire-When-Lay-Down/dp/0804105820/ref=sr_1_1?s=books&ie=UTF8&qid=1443327845&sr=1-1&keywords=It+was+on+Fire+When+I+Lay+Down+on+It" alt="link to Amazon" target="_blank"><strong>It was on Fire When I Lay Down on It</strong></a></li>
+									<li class="reading-menu"><a href="http://www.amazon.com/Where-Sidewalk-Ends-Poems-Drawings/dp/0060256672/ref=sr_1_1?s=books&ie=UTF8&qid=1443327909&sr=1-1&keywords=Where+the+Sidewalk+Ends" alt="link to Amazon" target="_blank"><strong>Where the Sidewalk Ends</strong></a></li>
 								</ul>
 						</nav><!-- .example-menu -->
 						</div>
@@ -74,6 +75,9 @@
 							Marketing, design, and development go hand-in-hand in building, launching, and selling a successful product.
 							Seeking understanding of the complexities of everyday human experience and merging them with business goals leads to a cognitive marketing approach to design.
 						</p>
+						<p class="paragraph">
+							Remember to keep it simple, make it useful, practice empathy, apply constraints, have fun, and don't take yourself to serious.
+						</p>
 					<div class="white-space"></div>
 				</article>
 			</div>
@@ -89,26 +93,22 @@
 		<div class="row">
 			<div class="col-md-4">
 				<div class="suggested-reading">
-					<h3>Suggested Reading</h3>
+					<h4>Leadership and Process:</h4>
 					<nav class="example-menu">
 						<ul>
-							<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
-							<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
-							<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
-							<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
-							<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
-							<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
-							<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
-							<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
-							<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
-							<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
-							<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
-							<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
-							<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
-							<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
-							<li class="reading-menu"><a href="../cognitive-marketing/index.php"><strong>Cognitive Marketing </strong></a></li>
-							<li class="reading-menu"><a href="../inbound-marketing/index.php"><strong>InBound Marketing</strong></a> </li>
-							<li class="reading-menu"><a href="../outbound-marketing/index.php"><strong>OutBound Marketing</strong></a></li>
+							<li class="reading-menu"><a href="http://www.amazon.com/Start-Why-Leaders-Inspire-Everyone/dp/1591846447/ref=sr_1_1?s=books&ie=UTF8&qid=1443328031&sr=1-1&keywords=Start+with+Why" alt="link to Amazon" target="_blank"><strong>Start with Why</strong></a> </li>
+							<li class="reading-menu"><a href="http://www.amazon.com/Leaders-Eat-Last-Together-Others/dp/1591845327/ref=sr_1_1?s=books&ie=UTF8&qid=1443328055&sr=1-1&keywords=Leaders+Eat+Last" alt="link to Amazon" target="_blank"><strong>Leaders Eat Last</strong></a></li>
+							<li class="reading-menu"><a href="http://www.amazon.com/Redesigning-Leadership-Simplicity-Technology-Business/dp/0262015889/ref=sr_1_1?s=books&ie=UTF8&qid=1443328075&sr=1-1&keywords=Redesigning+Leadership" alt="link to Amazon" target="_blank"><strong>Redesigning Leadership</strong></a></li>
+							<li class="reading-menu"><a href="http://www.amazon.com/StrengthsFinder-2-0-Tom-Rath/dp/159562015X/ref=sr_1_1?s=books&ie=UTF8&qid=1443328148&sr=1-1&keywords=Strengths+Finder" alt="link to Amazon" target="_blank"><strong>Strengths Finder</strong></a> </li>
+							<li class="reading-menu"><a href="http://www.amazon.com/Drive-Surprising-Truth-About-Motivates/dp/1594484805/ref=sr_1_1?s=books&ie=UTF8&qid=1443328168&sr=1-1&keywords=Drive" alt="link to Amazon" target="_blank"><strong>Drive</strong></a></li>
+							<li class="reading-menu"><a href="http://www.amazon.com/Habits-Highly-Effective-People-Powerful/dp/1451639619/ref=sr_1_1?s=books&ie=UTF8&qid=1443328193&sr=1-1&keywords=The+7+Habits+of+Highly+Effective+People" alt="link to Amazon" target="_blank"><strong>The 7 Habits of Highly Effective People</strong></a> </li>
+							<li class="reading-menu"><a href="http://www.amazon.com/Five-Love-Languages-Heartfelt-Commitment/dp/B006Q2LTRO/ref=sr_1_2?s=books&ie=UTF8&qid=1443328267&sr=1-2&keywords=The+Five+Love+Languages" alt="link to Amazon" target="_blank"><strong>The Five Love Languages</strong></a></li>
+							<li class="reading-menu"><a href="http://www.amazon.com/Goal-Process-Ongoing-Improvement/dp/0884271951/ref=sr_1_1?s=books&ie=UTF8&qid=1443328304&sr=1-1&keywords=The+Goal" alt="link to Amazon" target="_blank"><strong>The Goal</strong></a></li>
+							<li class="reading-menu"><a href="http://www.amazon.com/Its-Not-Luck-Eliyahu-Goldratt/dp/0884271153/ref=sr_1_1?s=books&ie=UTF8&qid=1443328394&sr=1-1&keywords=It%27s+not+luck" alt="link to Amazon" target="_blank"><strong>The Goal II: It's not luck</strong></a> </li>
+							<li class="reading-menu"><a href="http://www.amazon.com/Critical-Chain-Eliyahu-M-Goldratt/dp/0884271536/ref=sr_1_1?s=books&ie=UTF8&qid=1443328417&sr=1-1&keywords=Critical+Chain" alt="link to Amazon" target="_blank"><strong>Critical Chain</strong></a></li>
+							<li class="reading-menu"><a href="http://www.amazon.com/Logical-Thinking-Process-Systems-Approach/dp/0873897234/ref=sr_1_1?s=books&ie=UTF8&qid=1443328448&sr=1-1&keywords=The+Logical+Thinking+Process" alt="link to Amazon" target="_blank"><strong>The Logical Thinking Process</strong></a></li>
+							<li class="reading-menu"><a href="http://www.amazon.com/Lean-QuickStart-Guide-Simplified-Beginners/dp/0996366709/ref=sr_1_1?s=books&ie=UTF8&qid=1443328469&sr=1-1&keywords=Lean+Quickstart+Guide" alt="link to Amazon" target="_blank"><strong>Lean Quickstart Guide</strong></a></li>
+							<li class="reading-menu"><a href="http://www.amazon.com/Operations-Management-Dummies-Mary-Anderson/dp/1118551060/ref=sr_1_1?s=books&ie=UTF8&qid=1443328490&sr=1-1&keywords=Operations+Management+for+Dummies" alt="link to Amazon" target="_blank"><strong>Operations Management for Dummies</strong></a></li>
 						</ul>
 					</nav><!-- .example-menu -->
 				</div>
