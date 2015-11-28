586ad7883f4ea61be5435978c2d201460f100341
marie jt vigil <marie@mariejtvigil.com>
2015-11-27 17:46:47 -0700

    Not going to add portfolio gallery to this existing page, instead, redirecting viewers to new promotion website. -deleted portfolio gallery code from inbound page -put portfolio gallery code from inbound page into a separate document to save for another day - added html, css, and link to mariejtvigil.com website for inbound page portfolio gallery code examples

---
+++ inbound-marketing/portfolio-gallery.php
@@ -0,0 +1,156 @@
+<!-- Start Portfolio Content -->
+	<section id="content">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<!-- Start Row of Portfolio Views -->
+					<div class="row">
+						<section id="projects">
+							<ul id="thumbs" class="portfolio">
+								<!-- Start Project ONE -->
+								<li class="item-thumbs col-lg-3 design" data-id="id-0" data-type="web">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Project Title" href="../img/works/1.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb">Project<br> Title</span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="../img/works/1.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis." />
+								</li>
+								<!-- End Project ONE -->
+								<!-- Start Project TWO -->
+								<li class="item-thumbs col-lg-3 design" data-id="id-1" data-type="icon">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Project Title" href="../img/works/2.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb">Project<br> Title</span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="../img/works/2.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis." />
+								</li>
+								<!-- End Project TWO -->
+								<!-- Start Project THREE -->
+								<li class="item-thumbs col-lg-3 photography" data-id="id-2" data-type="graphic">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Project Title" href="../img/works/3.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb">Project<br> Title</span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="../img/works/3.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis." />
+								</li>
+								<!-- End Project THREE -->
+								<!-- Start Project FOUR -->
+								<li class="item-thumbs col-lg-3 design" data-id="id-0" data-type="web">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Project Title" href="../img/works/4.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb">Project<br> Title</span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="../img/works/4.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis." />
+								</li>
+								<!-- End Project FOUR -->
+								<!-- Start Project FIVE -->
+								<li class="item-thumbs col-lg-3 photography" data-id="id-4" data-type="web">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Project Title" href="../img/works/5.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb">Project<br> Title</span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="../img/works/5.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis." />
+								</li>
+								<!-- End Project FIVE -->
+								<!-- Start Project SIX -->
+								<li class="item-thumbs col-lg-3 photography" data-id="id-5" data-type="icon">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Project Title" href="../img/works/6.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb">Project<br> Title</span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="../img/works/6.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis." />
+								</li>
+								<!-- End Project SIX -->
+								<!-- Start Project SEVEN -->
+								<li class="item-thumbs col-lg-3 design" data-id="id-0" data-type="web">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Project Title" href="../img/works/7.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb">Project<br> Title</span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="../img/works/7.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis." />
+								</li>
+								<!-- End Project SEVEN -->
+								<!-- Start Project EIGHT -->
+								<li class="item-thumbs col-lg-3 design" data-id="id-0" data-type="graphic">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Project Title" href="../img/works/8.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb">Project<br> Title</span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="../img/works/8.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis." />
+								</li>
+								<!-- End Project EIGHT -->
+								<!-- Start Project NINE -->
+								<li class="item-thumbs col-lg-3 design" data-id="id-0" data-type="graphic">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Project Title" href="../img/works/8.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb">Project<br> Title</span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="../img/works/8.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis." />
+								</li>
+								<!-- End Project NINE -->
+								<!-- Start Project TEN -->
+								<li class="item-thumbs col-lg-3 design" data-id="id-0" data-type="graphic">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Project Title" href="../img/works/8.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb">Project<br> Title</span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="../img/works/8.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis." />
+								</li>
+								<!-- End Project TEN -->
+								<!-- Start Project ELEVEN -->
+								<li class="item-thumbs col-lg-3 design" data-id="id-0" data-type="graphic">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Project Title" href="../img/works/8.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb">Project<br> Title</span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="../img/works/8.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis." />
+								</li>
+								<!-- End Project ELEVEN -->
+								<!-- Start Project TWELVE -->
+								<li class="item-thumbs col-lg-3 design" data-id="id-0" data-type="graphic">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Project Title" href="../img/works/8.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb">Project<br> Title</span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="../img/works/8.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis." />
+								</li>
+								<!-- End Project TWELVE -->
+							</ul>
+							<!--  #thumbs .portfolio -->
+						</section>
+						<!-- projects -->
+					</div>
+					<!-- .row(s) of Portfolio Views-->
+				</div>
+				<!-- .col-lg-12 -->
+			</div>
+			<!-- .row -->
+		</div>
+		<!-- .container -->
+	</section>
+	<!-- #content -->
+	<!-- End Portfolio Content -->
