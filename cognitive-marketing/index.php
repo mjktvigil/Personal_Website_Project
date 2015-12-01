734aabdb5a1d40eb032513e9082b780324e95a6f
marie jt vigil <marie@mariejtvigil.com>
2015-12-01 11:02:53 -0700

    Adding Google Analytics to website pages

--- cognitive-marketing/index.php
+++ cognitive-marketing/index.php
@@ -1,5 +1,13 @@
 <?php require_once('../lib/head-utils.php'); ?>
 <?php require_once('../lib/header.php'); ?>
+	<!-- Google Tag Manager -->
+	<noscript>&lt;iframe src="//www.googletagmanager.com/ns.html?id=GTM-TRJWZ7" height="0" width="0" style="display:none;visibility:hidden"&gt;&lt;/iframe&gt;</noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-TRJWZ7');</script>
+	<!-- End Google Tag Manager -->
 	<!-- Start Eyeglass Image -->
 	<section id="contact-info">
 		<div class="cognitive-image">
