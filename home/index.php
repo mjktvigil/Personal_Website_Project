2bbc7668f61b6777988a1a84f17bb297f81e4440
marie jt vigil <marie@mariejtvigil.com>
2015-12-20 17:59:42 -0700

    added Google Tag Manager to website pages

--- home/index.php
+++ home/index.php
@@ -1,12 +1,13 @@
 <?php require_once('../lib/head-utils.php'); ?>
 <?php require_once('../lib/header.php'); ?>
 	<!-- Google Tag Manager -->
-	<noscript>&lt;iframe src="//www.googletagmanager.com/ns.html?id=GTM-TRJWZ7" height="0" width="0" style="display:none;visibility:hidden"&gt;&lt;/iframe&gt;</noscript>
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PVHX2F"
+							height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 			'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-TRJWZ7');</script>
+		})(window,document,'script','dataLayer','GTM-PVHX2F');</script>
 	<!-- End Google Tag Manager -->
 	<!-- Start Background Image -->
 	<div class="home-image">
